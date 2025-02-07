ff82156d907101f1526cf03fec3c97009e03c381
CambiUPR <sebastian.cambo@upr.edu>
2025-02-07 17:54:12 +0000

    Syncy 1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+// Asignacion 1: Binary Digit
+// Sebastian Cambo CambiUPR
+// 801-22-9348
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{   cout << "Este programa va a desplegar el beneficio neto y bruto en una noche de ventas en un cine. " << endl;
+
+// Aqui pedimos el titulo de la pelicula
+    cout << "Escriba el nombre de la pelicula: ";
+        string nombre_pelicula;
+            cin >> nombre_pelicula;
+
+    cout << "Ahora escriba cuantas taquillas se vendieron de adultos: ";
+        string taquillas_adulto;
+            cin >> taquillas_adulto;
+
+    cout << "Finalmente escribe cuantas taquillas se vendieron de ninos: ";
+        string taquillas_nino;
+            cin >> taquillas_nino;
+
+
+
+
+
+
+
+}
